8444db573ec89c0289e236631170c0cafcfa5670
manningjt <angela1905@hotmail.com>
2022-10-31 15:00:12 -0500

    I made changes to use aggregation i was using before inheritance which was wrong :D

--- src/Point.java
+++ src/Point.java
@@ -1,28 +1,21 @@
 public class Point {
 
-    public String latOrigin,
-            lngOrigin,
-            id_rideOrigin,
-            timestampOrigin,
-            latDestination,
-            lngDestination,
-            id_rideDestination,
-            timestampDestination;
+    public String lat,
+            lng,
+            lat_line2,
+            lng_line2;
 
-    public Point (String lat,String lng,String id_ride,String timestamp,String lat_line2,String lng_line2,String id_ride2,String timestamp_line2){
 
-        this.latOrigin = lat;
-        this.lngOrigin = lng;
-        this.id_rideOrigin = id_ride;
-        this.timestampOrigin = timestamp;
-        this.latDestination = lat_line2;
-        this.lngDestination = lng_line2;
-        this.id_rideDestination = id_ride2;
-        this.timestampDestination = timestamp_line2;
+    public Point (String lat,String lng,String lat_line2,String lng_line2){
 
+        this.lat = lat;
+        this.lng = lng;
+        this.lat_line2 =lat_line2;
+        this.lng_line2= lng_line2;
 
     }
 
 
 
 }
+
--- src/Segment.java
+++ src/Segment.java
@@ -1,59 +1,75 @@
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class Segment extends Point {
+public class Segment {
 
     private double U, distance, delta_time_hours;
-    private  int new_timestamp = Integer.parseInt(timestampOrigin);
-    private  float new_id_ride = Float.parseFloat(id_rideOrigin);
-    private  double new_lng = Double.parseDouble(lngOrigin);
-    private double new_lat = Double.parseDouble(latOrigin);
 
-    public Segment(String latOrigin,
-                   String lngOrigin,
-                   String id_rideOrigin,
-                   String timestampOrigin,
-                   String latDestination,
-                   String lngDestination,
-                   String id_rideDestination,
-                   String timestampDestination)
-    {
-        super(latOrigin, lngOrigin, id_rideOrigin, timestampOrigin,latDestination,lngDestination,id_rideDestination,timestampDestination);
+    private List<Point> points;
+    private String id_ride;
+    private String timestamp;
+    private String timestamp_line2;
 
+    ArrayList<Double> fare;
+    public Segment(List<Point> points, String id_ride, String timestamp, String timestamp_line2,ArrayList<Double> fare) {
+        this.points = points;
+        this.id_ride = id_ride;
+        this.timestamp = timestamp;
+        this.timestamp_line2 = timestamp_line2;
+        this.fare=fare;
 
     }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
     public void calculate_distance() {
+        int new_timestamp = Integer.parseInt(timestamp);
 
-        double new_lng_line2 = Double.parseDouble(lngDestination);
-        double new_lat_destination = Double.parseDouble(latDestination);
-        int    new_timestamp_line2 = Integer.parseInt((timestampDestination));
 
-        double delta_time_seconds = (new_timestamp_line2 - new_timestamp);
-        delta_time_hours = delta_time_seconds / 3600;
-        double delta_latitude = Math.toRadians(new_lat_destination-new_lat );
-        double delta_longitude = Math.toRadians(new_lng_line2 - new_lng);
-        double a = Math.sin(delta_latitude / 2) * Math.sin(delta_latitude / 2) +
-                Math.cos(Math.toRadians(new_lat)) * Math.cos(Math.toRadians(new_lat_destination)) *
-                        Math.sin(delta_longitude / 2) * Math.sin(delta_longitude / 2);
-        double c = 2 * Math.asin((Math.sqrt(a)));
-        int EARTH_RADIUS = 6371;
-        distance = (EARTH_RADIUS * c);
+        for (Point pnt : points) {
+            String latOrigin = pnt.lat;
+            String lngOrigin = pnt.lng;
+            String latDestination = pnt.lat_line2;
+            String lngDestination = pnt.lng_line2;
+            double new_lat = Double.parseDouble(latOrigin);
+            double new_lng = Double.parseDouble(lngOrigin);
+            double new_lat_destination = Double.parseDouble(latDestination);
+            double new_lng_line2 = Double.parseDouble(lngDestination);
+            int new_timestamp_line2 = Integer.parseInt((timestamp_line2));
+            double delta_time_seconds = (new_timestamp_line2 - new_timestamp);
+            delta_time_hours = delta_time_seconds / 3600;
+            double delta_latitude = Math.toRadians(new_lat_destination - new_lat);
+            double delta_longitude = Math.toRadians(new_lng_line2 - new_lng);
+            double a = Math.sin(delta_latitude / 2) * Math.sin(delta_latitude / 2) +
+                    Math.cos(Math.toRadians(new_lat)) * Math.cos(Math.toRadians(new_lat_destination)) *
+                            Math.sin(delta_longitude / 2) * Math.sin(delta_longitude / 2);
+            double c = 2 * Math.asin((Math.sqrt(a)));
+            int EARTH_RADIUS = 6371;
+            distance = (EARTH_RADIUS * c);
+        }
     }
 
-    public double calculate_speed(){
+    public double calculate_speed() {
         U = (distance / delta_time_hours);
-       return U;
+        return U;
     }
-    public void validatePoint(String id_ride3, String lat_line3, String lng_line3, String timestamp_line3) {
-
-            latDestination = lat_line3;
-            lngDestination = lng_line3;
-            timestampDestination = timestamp_line3;
+    public void validatePoint(String lat_line3, String lng_line3, String timestamp_line3){
+        for (Point pnt : points) {
+            pnt.lat_line2 =lat_line3;
+            pnt.lng_line2 = lng_line3;
+        }
+        timestamp_line2 = timestamp_line3;
 
     }
-    public void fare_rules(ArrayList<Double> fare, ArrayList<Double> id_ride_final, int i) {
+    public void fare_rules(ArrayList<Double> id_ride_final, int i) {
 
+        float new_id_ride = Float.parseFloat(id_ride);
+        int new_timestamp = Integer.parseInt(timestamp);
         double fare_amount = 0.0;
         // when the speed is less than 10KM
         if (U <= 10) {
@@ -73,9 +89,9 @@ public class Segment extends Point {
         Date date_and_hour = new Date( time * 1000 );
         SimpleDateFormat sdf = new SimpleDateFormat("HHmmss");
         String Hour = sdf.format(date_and_hour);
-        String hour1 = new String("000000");
-        String hour2 = new String("050000");
-        String hour3 = new String("235959");
+        String hour1 = "000000";
+        String hour2 = "050000";
+        String hour3 = "235959";
 
         // when the speed is bigger than 10KM and the movement of the vehicle is between 00 hour and 5 AM
         if (U > 10 && Hour.compareTo(hour1) > 0 && Hour.compareTo(hour2) < 0) {
@@ -92,4 +108,7 @@ public class Segment extends Point {
             fare.add(i, fare_amount);
         }
     }
+
 }
+
+
